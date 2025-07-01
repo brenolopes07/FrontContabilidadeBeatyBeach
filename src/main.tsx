29bd56d3152b437f239b29063b6e1aeda04ee3d0
brenolopes07 <breno.skolegas07@gmail.com>
2025-07-01 02:06:35 -0300

    feat: configurated main router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import {App} from './App.tsx'
+import { RouterProvider } from 'react-router'
+import { router } from './router/index.tsx'
+import * as React from 'react'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
