e8d1cd9021372b9e69a5923a2904bc8c39ab24c6
brenolopes07 <breno.skolegas07@gmail.com>
2025-07-02 01:33:52 -0300

    feat: install react query and configuration

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,13 @@ import './index.css'
 import { RouterProvider } from 'react-router'
 import { router } from './router/index.tsx'
 import * as React from 'react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
+const queryClient = new QueryClient()
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>    
   </React.StrictMode>
 )
