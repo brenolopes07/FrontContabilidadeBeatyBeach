1ca5a0b3e9d2bcef1656ba56ef0bb6fe09aa4d3a
brenolopes07 <breno.skolegas07@gmail.com>
2025-07-02 01:36:23 -0300

    fix: Page Auth in routes

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,7 +1,8 @@
+import { Auth } from "@/pages/auth";
 import { createBrowserRouter } from "react-router-dom";
 
 export const router = createBrowserRouter([{
    path: "/",
-   element: <LoginPage/>,
+   element: <Auth/>,
 },
 ]);
