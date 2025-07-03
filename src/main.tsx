94f2dd197d7e3744db7142521a2279ab01eeff2f
brenolopes07 <breno.skolegas07@gmail.com>
2025-07-03 01:32:20 -0300

    feat: add Toaster

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,14 @@ import { RouterProvider } from 'react-router'
 import { router } from './router/index.tsx'
 import * as React from 'react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Toaster } from 'sonner'
 
 const queryClient = new QueryClient()
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
+      <Toaster/>
     </QueryClientProvider>    
   </React.StrictMode>
 )
