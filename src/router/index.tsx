7969d0fe24d2fe80cd58618e8e5764620910b4f5
brenolopes07 <breno.skolegas07@gmail.com>
2025-07-04 02:06:31 -0300

    feat: add initial dashboard page development

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,8 +1,13 @@
 import { Auth } from "@/pages/auth";
+import { Dashboard } from "@/pages/dashboard";
 import { createBrowserRouter } from "react-router-dom";
 
 export const router = createBrowserRouter([{
    path: "/",
    element: <Auth/>,
 },
+{
+   path: "/dashboard",
+   element: <Dashboard/>, 
+}
 ]);
