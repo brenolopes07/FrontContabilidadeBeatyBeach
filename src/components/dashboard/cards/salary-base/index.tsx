249123a8b52f6c6b5646dd5237795756f89cc1bb
brenolopes07 <breno.skolegas07@gmail.com>
2025-07-17 20:38:20 -0300

    feat: implement salaybase in the component

--- src/components/dashboard/cards/salary-base/index.tsx
+++ src/components/dashboard/cards/salary-base/index.tsx
@@ -1,18 +1,18 @@
 import { Card, CardContent } from "@/components/ui/card"
 import { DollarSign } from "lucide-react"
+import { salaryBaseData } from "./hooks/salary-base.usecase"
 
-interface SalaryBaseCardProps {
-   value: string // Ex: "R$ 5.000,00"
-}
 
-export function SalaryBaseCard({ value }: SalaryBaseCardProps) {
+export function SalaryBaseCard() {
+ const salario = salaryBaseData();
+
    return (
       <Card className="bg-gradient-to-r from-purple-500 to-purple-600 text-white">
          <CardContent className="p-4">
             <div className="flex items-center justify-between">
                <div>
                   <p className="text-sm opacity-90 font-medium">Salário Base</p>
-                  <p className="text-xl font-bold sm:text-2xl">{value}</p>
+                  <p className="text-xl font-bold sm:text-2xl">{salario !== null ? `R$ ${salario.toFixed(2)}` : "Carregando..."}</p>
                </div>
                <DollarSign className="w-8 h-8 opacity-80" />
             </div>
